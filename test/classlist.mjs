e737e587d587ef03759a1207f4fa99702f5d45db
matfax <matthias.fax@gmail.com>
2020-08-29 17:07:07 +0200

    refactor: migrate tests and overall project into es6 modules

---
+++ test/classlist.mjs
@@ -0,0 +1,28 @@
+import test from 'supertape'
+import { one, parent, TestCase, testLang, three, two } from './shared'
+
+
+
+export default (lang) => {
+  test('select class list', async (assert) => {
+    const data = [
+      { class: 'a-class   b-class  c-class ' },
+      { class: ['a-class', 'b-class', 'c-class'] }
+    ]
+
+    data.forEach((data) => {
+      testLang(assert, lang, [
+        new TestCase('.a-class', data),
+        new TestCase('.b-class', data),
+        new TestCase('.c-class', data),
+        new TestCase('.one-other-class', data, false)
+      ])
+    })
+
+    testLang(assert, lang, [
+      new TestCase('.whatever', { class: null }, false)
+    ])
+
+    assert.end()
+  })
+}
